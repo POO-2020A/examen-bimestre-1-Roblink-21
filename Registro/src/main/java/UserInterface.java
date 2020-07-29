5e5c4481617f80e5f46b5e281c9be368109d54ce
Roberth Pincha <58041699+Roblink-21@users.noreply.github.com>
2020-07-29 13:51:58 -0500

    Update UserInterface.java

--- Registro/src/main/java/UserInterface.java
+++ Registro/src/main/java/UserInterface.java
@@ -2,43 +2,39 @@
 import java.util.Scanner;
 
 public class UserInterface {
+
     private Scanner scanner;
     private Plane plano;
     private Seat ident;
-    
-    
-    
-    public UserInterface(Scanner scanner)
-    {
+
+    public UserInterface(Scanner scanner) {
         this.scanner = scanner;
-        
+
     }
-    
-    public void start()
-    {
+
+    public void start() {
+        Passenger persona = null;
+        Seat asignacion = null;
+
         System.out.println("Ingrese la cantidad de filas:");
         int filas = Integer.valueOf(scanner.nextLine());
         System.out.println("Ingrese la cantidad de columnas:");
         int colum = Integer.valueOf(scanner.nextLine());
-        
-        if((filas > 20) || (filas < 0))
-        {
+
+        if ((filas > 20) || (filas < 0)) {
             System.out.println("Ingrese de nuevo (Max: 20 y no se aceptan numeros menores a 0):");
             filas = Integer.valueOf(scanner.nextLine());
-        }
-        else if((colum > 20) || (colum < 0))
-        {
+        } else if ((colum > 20) || (colum < 0)) {
             System.out.println("Ingrese de nuevo (Max: 8 y no se aceptan numeros menores a 0):");
             filas = Integer.valueOf(scanner.nextLine());
         }
-        
+
         plano = new Plane(filas, colum);
-        
+
         System.out.println(plano);
         System.out.println("");
-        
-        while(true)
-        {
+
+        while (true) {
             //System.out.println("What a joke!");
             System.out.println("¿Qué desea hacer?");
             System.out.println(" 1. Ingresar pasajero");
@@ -46,65 +42,56 @@ public class UserInterface {
             System.out.println(" 3. Consultar pasajero");
             System.out.println(" X - Salir");
             System.out.println();
-            
-            
+
             System.out.print("Ingrese la opcion: ");
             String word = scanner.nextLine();
-            
-            if(word.equals("X"))
-            {
+
+            if (word.equals("X")) {
                 break;
-            }
-            else if(word.equals("1"))
-            {
+            } else if (word.equals("1")) {
                 System.out.println("Ingresar un pasajero:");
                 System.out.print("Ingrese el nombre del pasajero: ");
                 String name = scanner.nextLine();
                 System.out.print("Ingrese el apellido del pasajero: ");
                 String lastname = scanner.nextLine();
-                
+
                 System.out.print("Ingrese la fila del asiento: ");
                 int numb = Integer.valueOf(scanner.nextLine());
-                
+
                 System.out.print("Ingrese la columna del asiento: ");
                 String asiento = scanner.nextLine();
-                
-                Passenger persona = new Passenger(name, lastname);
-                Seat asignacion = new Seat(numb, asiento);
-                
+
+                persona = new Passenger(name, lastname);
+                asignacion = new Seat(numb, asiento, persona);
+
                 plano.seatSets(asignacion);
-                
-                
+
                 System.out.println("¡Pasajero ingresado!");
-                
+
                 System.out.println(plano);
-                
-                
-                
-            }
-            else if(word.equals("2"))
-            {
+
+            } else if (word.equals("2")) {
                 System.out.println("Consulta de asiento.");
                 System.out.print("Ingrese la fila del asiento: ");
                 int nb = Integer.valueOf(scanner.nextLine());
-                
+
                 System.out.print("Ingrese la columna del asiento: ");
                 String asit = scanner.nextLine();
-                
-                Seat verif = new Seat(nb, asit);
-                
-                verif.isFree();
-                System.out.println(verif.toString());
-                        
-                
-                
-                
+
+                for (Seat p : plano.listado()) {
+                    if (p.getRow() == nb && p.getCol().equals(asit)) {
+                        System.out.println(p);
+                    }
+                }
+
+            } else if (word.equals("3")) {
+                System.out.println("Consulta de pasajero.");
+                System.out.print("Ingrese el nombre y apellido: ");
+                String datos = scanner.nextLine();
+                String partes[] = datos.split(" ");
+
             }
-            else //if(word.equals("3"))
-            {
-                
-            }
-              
+
         }
-}
+    }
 }
